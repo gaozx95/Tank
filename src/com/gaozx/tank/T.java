3d7376f9f38cb85a977d764af2c1eaf94f2af058
gaozx95 <602753653@qq.com>
2019-10-03 11:13:09 +0800

    继承frame方法

--- src/com/gaozx/tank/T.java
+++ src/com/gaozx/tank/T.java
@@ -1,22 +1,8 @@
 package com.gaozx.tank;
 
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
 public class T {
     public static void main(String[] args) {
-        Frame f = new Frame();
-        f.setSize(800,600);
-        f.setResizable(false);
-        f.setTitle("Tank War");
-        
-        f.setVisible(true);
-        f.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        TankFrame f = new TankFrame();
+
     }
 }
