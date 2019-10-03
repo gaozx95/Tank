5900864b8ccfb7c118e003eed2e1f0203c7ee1e6
gaozx95 <602753653@qq.com>
2019-10-03 17:25:41 +0800

    双缓冲解决闪烁问题

--- src/com/gaozx/tank/TankFrame.java
+++ src/com/gaozx/tank/TankFrame.java
@@ -7,6 +7,8 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
+    private static final int GAME_WIDTH = 800,GAME_HEIGHT = 600;
+
     Tank myTank = new Tank(200,200,Dir.DOWN);
     Bullet b = new Bullet(300,300,Dir.DOWN);
     //构造方法
@@ -14,7 +16,6 @@ public class TankFrame extends Frame {
         setSize(800,600);
         setResizable(false);
         setTitle("Tank War");
-
         setVisible(true);
         addWindowListener(new WindowAdapter() {
             @Override
@@ -24,6 +25,23 @@ public class TankFrame extends Frame {
         });
         this.addKeyListener(new MyKeyListener());
     }
+
+    Image offScreenImage = null;
+    @Override
+    public void update(Graphics g){
+        if(offScreenImage == null){
+            offScreenImage = this.createImage(GAME_WIDTH,GAME_HEIGHT);
+        }
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.BLACK);
+        gOffScreen.fillRect(0,0,GAME_WIDTH,GAME_HEIGHT);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage,0,0,null);
+
+    }
+
     //paint 方法自动调用
     @Override
     public void paint(Graphics g){
