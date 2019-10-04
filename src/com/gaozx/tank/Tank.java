8989cdb9433be854a8ef09fc3e1ba168cf711e91
gaozx95 <602753653@qq.com>
2019-10-04 11:08:11 +0800

    建立resourcemgr类管理资源，把图片load到内存，坦克换成图片

--- src/com/gaozx/tank/Tank.java
+++ src/com/gaozx/tank/Tank.java
@@ -34,10 +34,22 @@ public class Tank {
     }
 
     public void paint(Graphics g) {
-        Color c = g.getColor();
-        g.setColor(Color.GREEN);
-        g.fillRect(x,y,WIDTH,HEIGHT);
-        g.setColor(c);
+        //画图片
+        switch (dir){
+            case LEFT:
+                g.drawImage(ResourceMgr.tankL,x,y,null);
+                break;
+            case UP:
+                g.drawImage(ResourceMgr.tankU,x,y,null);
+                break;
+            case RIGHT:
+                g.drawImage(ResourceMgr.tankR,x,y,null);
+                break;
+            case DOWN:
+                g.drawImage(ResourceMgr.tankD,x,y,null);
+                break;
+        }
+
         move();
 
     }
