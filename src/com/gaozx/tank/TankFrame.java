660c3bb3c4b2a237d9e1159d0bdeeba26975eeba
gaozx95 <602753653@qq.com>
2019-10-06 10:00:31 +0800

    class.forname.newInstance新方法

--- src/com/gaozx/tank/TankFrame.java
+++ src/com/gaozx/tank/TankFrame.java
@@ -14,7 +14,9 @@ public class TankFrame extends Frame {
 
 
     Tank myTank = new Tank(200,400,Dir.DOWN,Group.GOOD,this);
-
+    {
+        myTank.setMoving(false);
+    }
     List<Bullet> bullets = new ArrayList<>();
     List<Tank> tanks = new ArrayList<>();
     List<Explode> explodes = new ArrayList<>();
